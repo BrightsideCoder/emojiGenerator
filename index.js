3a5ceef7fe00a264c9a5817cdcf72a814bfe284d
Torsten Krüger <torsten.kruger90@gmail.com>
2022-12-29 22:25:56 +0100

    Tihi

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// Grab the emojiContainer div and store it in a variable. Log it to the console
-
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 
 for (let i = 0; i < myEmojis.length; i++) {
-    // console.log(myEmojis[i])
+    console.log(myEmojis[i])
 }
